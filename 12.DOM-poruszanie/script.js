db1bb8789c6b65488b10b2c26472e9161ffecc94
Scored <multiusage2@o2.pl>
2020-10-10 18:37:21 +0200

    Day 5.2

--- 12.DOM-poruszanie/script.js
+++ 12.DOM-poruszanie/script.js
@@ -1,23 +1,87 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    // Zadanie 1.
-    //a
-    function parentFinder(){
-    var x = document.querySelector("#buz").parentElement;
-    return x;
-    }
-    console.log(parentFinder());
 
+    // Zadanie 1.
+
+    //a
+    console.log(document.querySelector('#buz').parentElement);
     //b
-    function siblingFinder(){
-        var x = document.querySelector("#baz").previousElementSibling;
-        return x;
-        }
-        console.log(siblingFinder());
-    //c 
-    function childrenFinder(){
-        var x = document.querySelector("#foo").children;
-        return x;
-        }
-        console.log(siblingFinder());
+    console.log(document.querySelector('#baz').previousElementSibling);
+    //c
+    console.log(document.querySelector('#foo').children);
+    //d
+    console.log(document.querySelector('#foo').parentElement);
+    //e
+    console.log(document.querySelector('#foo').children[0]);
+    //f
+    console.log((function() {
+        let foo = document.querySelector('#foo');
+        let numOfChilds = foo.children.length -1;
+        let middleElement = Math.round(numOfChilds / 2);
+        return foo.children[middleElement];
+    })());
+
+    // Zadanie 2.
+    let addEvent = function(elementId) {
+        let element = document.querySelector(`#${elementId}`);
+        element.addEventListener('click', function(event) {
+            console.log(event.target.textContent.trim());
+        });
+    }
+    addEvent('ex2');
+
+    // Zadanie 3.
+    document.querySelector('#ex3').querySelectorAll('button').forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.target.nextElementSibling.style.display = event.target.nextElementSibling.style.display == 'none' ? 'block' : 'none';
+        });
+    });
+
+    // Zadanie 4.
+    document.querySelector('#ex3').querySelectorAll('button').forEach((element) => {
+        element.addEventListener('click', (event) => {
+            let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            event.target.parentElement.style.backgroundColor = randomColor;
+        });
+    });
+
+    //Zadanie 5 
+    let ex5 = document.querySelector('#ex5');
+    let divs = ex5.querySelectorAll('div');
+    let list = ex5.querySelectorAll('ul>li');
+
+    //a
+    function a(e) {
+    list[0].style.backgroundColor = e.srcElement.style.backgroundColor;
+    }
+    //b
+    function b(e) {
+    list[list.length-1].style.backgroundColor = e.srcElement.style.backgroundColor;
+    }
+    //c
+    function c(e) {
+    Array.from(list)
+        .filter((value) => value.innerHTML % 2 === 0)
+        .forEach((li) => li.style.backgroundColor = e.srcElement.style.backgroundColor)
+    }
+    //d
+    function d(e) {
+    list.forEach((li) => li.style.backgroundColor = e.srcElement.style.backgroundColor)
+    }
+    //e
+    function e(e) {
+    ex5.querySelector('ul').style.backgroundColor = e.srcElement.style.backgroundColor;
+    }
+
+    divs.forEach((div) => {
+    div.addEventListener('mouseover', d)});
+    
+
+    // Zadanie 6.
+
+    //a
+    let val  = document.querySelector('#ex6').firstElementChild.firstElementChild.firstElementChild;
+    //b
+    let val2 = document.querySelector('#ex6').firstElementChild.parentElement.firstElementChild.firstElementChild.nextElementSibling.parentElement;
+    //c
+    let val3 = document.querySelector('#ex6').parentElement.firstElementChild.parentElement.children[1].firstElementChild.firstElementChild.firstElementChild;
 });
