492fc092dc40b5cd9c2926ebe2ea587d770b0973
Scored <multiusage2@o2.pl>
2020-10-12 12:13:06 +0200

    Day 6

--- 13.DOM-eventy/script.js
+++ 13.DOM-eventy/script.js
@@ -1,26 +1,88 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-//Zadanie 1
+    //Zadanie 1
+    document.querySelector('#test-event').addEventListener('click', (event) => console.log(event));
+    document.addEventListener('mousemove', (event) => console.log(event));
+    document.querySelector('#test-event').addEventListener('mouseover', (event) => console.log(event));
+    document.addEventListener('keypress', (event) => console.log(event));
+    document.addEventListener('scroll', (event) => console.log(event));
+    document.querySelector('#input-test').addEventListener('change', (event) => console.log(event));
+    //Zadanie 2
 
-//Zadanie 2
+    document.querySelector('#ex2').addEventListener("click", function(){
+        let element = document.querySelector('#ex2');
+        let dataAttribute = element.dataset.text;
+        let span = document.querySelector('#span-ex2')
+        span.innerHTML = dataAttribute;
+    });
 
-document.querySelector('#ex2').addEventListener("click", function(){
-    let element = document.querySelector('#ex2');
-    let dataAttribute = element.dataset.text;
-    let span = document.querySelector('#span-ex2')
-    span.innerHTML = dataAttribute;
-});
+    //Zadanie 3
+    document.querySelector('#ex3').addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'blue');
+    document.querySelector('#ex3').addEventListener('mouseout', (event) => event.target.style.backgroundColor = 'red');
+    // nie udało mi się zrobić jedną funkcją
 
-//Zadanie 3
-document.querySelector('#ex3').addEventListener("mouseover", function(event){
-    event.target.style.backgroundColor  = "blue";
-});
-//Zadanie 4 
+    //Zadanie 4 
+    function inputTest(target) {
+        let input = target.value;
+        let re = /^[a-zA-Z]+$/
 
-//Zadanie 5 
+        return re.test(input);
+    }
+    document.querySelector('#input-test').addEventListener('change', (event) => {
+        let isValid = inputTest(event.target);
+        let errorSpan = document.querySelector('#ex3-err');
 
-//Zadanie 6
+        if (isValid == false) {
+            errorSpan.innerText = "Only letters allowed.";
+            return;
+        }
+        errorSpan.innerText = "";
+    });
 
-//Zadanie 7
+    //Zadanie 5 
+    let counter = 0;
 
+    function click (event) {
+        let target = event.target;
+        document.querySelector('#ex5').innerText = ++counter;
+        if (counter >= 10) {
+            target.removeEventListener('click', click);
+        }
+    }
+    document.querySelector('#ex5-button').addEventListener('click', click);
+
+    //Zadanie 6
+    document.addEventListener('scroll', (event) => {
+        let position = window.scrollY;
+        let body = document.querySelector('body');
+
+        if (position > 200) {
+            body.style.backgroundColor = 'red';
+        } else {
+            body.style.backgroundColor = 'white';
+        }
+    });
+
+    //Zadanie 7
+    let calcInput = document.querySelector('#calculator > input');
+    let nextClickIsClear = false;
+    let nextClick = false;
+
+    document.querySelectorAll('#calculator > div > button').forEach((element) => {
+        element.addEventListener('click', (event) => {
+            if (nextClickIsClear == true) {
+                calcInput.value = "";
+                nextClickIsClear = false;
+            }
+            calcInput.value = calcInput.value + event.target.innerText;
+            if (nextClick == true) {
+                calcInput.value = eval(calcInput.value);
+                nextClick = false;
+                nextClickIsClear = true;
+            }
+            if (['/', '*', '+', '-'].includes(event.target.innerText)) {
+                nextClick = true;
+            }
+        });
+    });
 });
